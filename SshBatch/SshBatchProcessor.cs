d57240a075d197b5a67212bf206de2e99103aa63
Mateu Cerqueira <43365328+mcerqueira1509@users.noreply.github.com>
2019-11-20 15:02:00 +0000

    Created CheckInitialConfiguration
    Created CheckInitialConfiguration Method

--- SshBatch/SshBatchProcessor.cs
+++ SshBatch/SshBatchProcessor.cs
@@ -14,37 +14,54 @@ namespace SshBatch
         
         private readonly IFileReader fileReader;
         private readonly ISsh ssh;
+        private string errorMessage;
+        private string[] lines;
+        private string[] configLine;
 
         public string ProcessParams(params string[] args)
         {
-            string[] lines;
-            string[] configLine;
-            string result = "";
+            if (!CheckInitialConfiguration(args))
+                return HelpText(errorMessage);
 
-            if (args.Length == 0)
-                return HelpText("Error: batch filename was not in parameters.");
-            else if ((new string[] { "-h", "--h", "-help", "help" }).Contains(args[0].ToLower()))
-                return HelpText();
-            else if (!fileReader.Exists(args[0]))
-                return HelpText(string.Format("Error: batch file {0} does not exist.", args[0]));
-            else 
-            {
-                lines = fileReader.ReadAllLines(args[0]);
-                if (lines.Length < 1)
-                    return HelpText("Error: batcth file was empty.");
-                configLine = lines[0].Split(' ');
-                if (configLine.Length < 4)
-                    return HelpText("Error: batch file with invalid arguments.");
-            }
             ssh.Setup(configLine[0], configLine[1], configLine[2], configLine[3]);
             ssh.Connect();
+            string result = "";
             for (int i = 1; i < lines.Length; i++)
                 result += ssh.Command(lines[i]);
             ssh.Disconnect();
-
             return result;
         }
 
+        private bool CheckInitialConfiguration(string[] args)
+        {
+            if (args.Length == 0)
+                return SetErroMessage("Error: batch filename was not in parameters.");
+            else if (IsAskingHelp(args))
+                return SetErroMessage("");
+            else if (!fileReader.Exists(args[0]))
+                return SetErroMessage(string.Format("Error: batch file {0} does not exist.", args[0]));
+            else
+            {
+                lines = fileReader.ReadAllLines(args[0]);
+                if (lines.Length < 1)
+                    return SetErroMessage("Error: batcth file was empty.");
+                configLine = lines[0].Split(' ');
+                if (configLine.Length < 4)
+                    return SetErroMessage("Error: batch file with invalid arguments.");
+            }
+            return true;
+        }
+
+        private static bool IsAskingHelp(string[] args)
+        {
+            return (new string[] { "-h", "--h", "-help", "help" }).Contains(args[0].ToLower());
+        }
+
+        private bool SetErroMessage(string text)
+        {
+            errorMessage = text;
+            return false;
+        }
 
         private static string HelpText(string text = null)
         {
